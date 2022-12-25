1923353dcedccf5131824d56b518fa08e2681c59
Alexander Juhn <software@juhn.net>
2022-12-25 23:46:15 +0100

    HI-65 Fixed typo in name

--- frontend/inventarFrontend/README.md
+++ frontend/inventarFrontend/README.md
@@ -1,4 +1,4 @@
-# InvetarFrontend
+# inventarFrontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.3.
 
--- frontend/inventarFrontend/src/app/app.component.spec.ts
+++ frontend/inventarFrontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'invetarFrontend'`, () => {
+  it(`should have as title 'inventarFrontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invetarFrontend');
+    expect(app.title).toEqual('inventarFrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('invetarFrontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('inventarFrontend app is running!');
   });
 });
