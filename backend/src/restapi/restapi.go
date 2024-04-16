6475fe280030fde5ef37d80678af2f0d355e859b
Alexander Juhn <software@juhn.net>
2024-04-16 23:07:53 +0200

    HI-35 Removed Datadog librarys

--- backend/src/restapi/restapi.go
+++ backend/src/restapi/restapi.go
@@ -6,60 +6,51 @@ import (
 	"net/http"
 	"strings"
 
-    gintrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gin-gonic/gin"
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func StartServer() {
-    // tracer stuff
-    tracer.Start(tracer.WithAnalytics(true))
-	defer tracer.Stop()
+	dc.ReadConfig()
+	router := gin.Default()
 
-    dc.ReadConfig()
-    router := gin.Default()
+	// CORS
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowHeaders = []string{"Origin", "Item-Id", "*"}
+	router.Use(cors.New(config))
 
-    router.Use(gintrace.Middleware("inventar-backend"))
-
-    // CORS
-    config := cors.DefaultConfig()
-    config.AllowAllOrigins = true
-    config.AllowHeaders = []string{"Origin","Item-Id","*"}
-    router.Use(cors.New(config))
-
-    router.GET("/inventar_backend/readInventar", getInventar)
-    router.POST("/inventar_backend/updateInventar", updateInventar)
-    router.GET("/inventar_backend/livenessProbe", livenessProbe)
-    router.GET("/inventar_backend/getItemHistory", getItemHistory)
-    router.Run(":8080")
+	router.GET("/inventar_backend/readInventar", getInventar)
+	router.POST("/inventar_backend/updateInventar", updateInventar)
+	router.GET("/inventar_backend/livenessProbe", livenessProbe)
+	router.GET("/inventar_backend/getItemHistory", getItemHistory)
+	router.Run(":8080")
 }
 
 func getInventar(c *gin.Context) {
-    var itemList []dc.Item = dc.ReadInventar()
+	var itemList []dc.Item = dc.ReadInventar()
 
-    c.IndentedJSON(http.StatusOK, itemList)
+	c.IndentedJSON(http.StatusOK, itemList)
 }
 
 func updateInventar(c *gin.Context) {
-    var updatedItem []dc.Item
-    if err := c.BindJSON(&updatedItem); err != nil {
-        fmt.Println(err)
-        return
-    }
+	var updatedItem []dc.Item
+	if err := c.BindJSON(&updatedItem); err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    dc.UpdateInventar(updatedItem)
+	dc.UpdateInventar(updatedItem)
 }
 
 func getItemHistory(c *gin.Context) {
-    itemId:= c.Request.URL.Query()["itemId"]
-    //itemId := c.Request.Header["Item-Id"]
-    itemHistory := dc.GetItemHistory(strings.Join(itemId,""))
+	itemId := c.Request.URL.Query()["itemId"]
+	//itemId := c.Request.Header["Item-Id"]
+	itemHistory := dc.GetItemHistory(strings.Join(itemId, ""))
 
-    c.IndentedJSON(http.StatusOK, itemHistory)
+	c.IndentedJSON(http.StatusOK, itemHistory)
 }
 
-func livenessProbe(c *gin.Context){
-    c.IndentedJSON(200,"Ok")
+func livenessProbe(c *gin.Context) {
+	c.IndentedJSON(200, "Ok")
 }
